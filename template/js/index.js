77ee93ef9fa07fb5ffd1ae6ef167a47cd4810d08
Philip Newman <philipeano@gmail.com>
2017-07-13 14:54:11 +0100

    Install ESLint

---
+++ template/js/index.js
@@ -0,0 +1,15 @@
+
+export default function () {
+  const registerUser = () => {
+    let successMessage = 'Registration successful!';
+    return successMessage;
+  };
+  const loginUser = () => {
+    let successMessage = 'Login successful!';
+    return successMessage;
+  };
+  const logOutUser = () => {
+    let successMessage = 'Logout successful!';
+    return successMessage;
+  };
+}
