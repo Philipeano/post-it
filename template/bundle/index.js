d8d6d71b173c90d49e285de64b5f61a8e6cb2d57
Philip Newman <philipeano@gmail.com>
2017-07-17 12:25:13 +0100

    Clean up template files

---
+++ template/bundle/index.js
@@ -0,0 +1,20 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+exports.default = function () {
+  var registerUser = function registerUser() {
+    var successMessage = 'Registration successful!';
+    return successMessage;
+  };
+  var loginUser = function loginUser() {
+    var successMessage = 'Login successful!';
+    return successMessage;
+  };
+  var logOutUser = function logOutUser() {
+    var successMessage = 'Logout successful!';
+    return successMessage;
+  };
+};
--- template/webpack.config.js
+++ template/webpack.config.js
@@ -1,20 +1,38 @@
-const path = require('path');
-const webpack = require('webpack');
-
-const DIST_DIR   = path.join(__dirname, 'bundle');
-const CLIENT_DIR = path.join(__dirname, 'js');
-
 module.exports = {
-    context: CLIENT_DIR,
+  entry: './js/index.js',
+  output: {
+    path: './bundle',
+    filename: 'bundle.js',
+    resolve: { extensions: ['', '.js', '.jsx'] },
+  },
+  module: {
+    loaders: [{
+      test: /\.js$/,
+      exclude: /node_modules/,
+      loader: 'babel-loader'
+    }]
+  }
+}
 
-    entry: 'index.js',
 
-    output: {
-        path:     DIST_DIR,
-        filename: 'bundle.js'
-    },
+// const path = require('path');
+// const webpack = require('webpack');
+//
+// const DIST_DIR   = path.join(__dirname, 'bundle');
+// const CLIENT_DIR = path.join(__dirname, 'js');
+//
+// module.exports = {
+//     context: CLIENT_DIR,
+//
+//     entry: 'index.js',
+//
+//     output: {
+//         path:     DIST_DIR,
+//         filename: 'index.js'
+//     },
+//
+//     resolve: {
+//         extensions: ['', '.js', '.jsx']
+//     }
+// };
 
-    resolve: {
-        extensions: ['', '.js', '.jsx']
-    }
-};
