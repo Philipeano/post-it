cdaeb494d40fa3d00114abd9438538f04fd8d246
Philip Newman <philipeano@gmail.com>
2017-07-18 12:36:44 +0100

    Clean up server files

--- server/app.js
+++ server/app.js
@@ -1,8 +1,17 @@
+// const express = require('express');
+// const logger = require('morgan');
+// const bodyParser = require('body-parser');
 import express from 'express';
 import logger from 'morgan';
 import bodyParser from 'body-parser';
+import dotenv from 'dotenv';
+// TODO: import all routes
+// TODO: import all models
 
-// Set up the express app
+// Configure environment settings
+dotenv.config();
+
+// Set up server express
 const app = express();
 
 // Log requests to the console
@@ -12,9 +21,25 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-// Default route
-app.get('*/api/*', (req, res) => res.status(200).send({
-  message: 'Welcome! PostIT API is running...',
-}));
+// TODO: Set up middleware
+// TODO: Set up authentication routes
+// TODO: Set up all other routes
 
-export default app;
+// Default/random route
+app.use('/*', (req, res) => {
+  res.status(200).send({ message: 'Welcome! PostIT API is running...' })
+});
+
+// Retrieve port for this app environment
+const port = parseInt(process.env.PORT, 10) || 8000;
+
+// TODO: Run Sequelize sync on models
+
+// Create server and initialize it with the express app
+const server = app.listen(port, () => {
+  console.log(`Listening at port ${port}`);
+});
+
+// Export server
+export default server;
+// module.exports = server;
