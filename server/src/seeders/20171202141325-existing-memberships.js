4414ccd787d00527d0b6a041a210e8ffc6587409
Philipeano <philipeano@gmail.com>
2017-12-02 18:26:56 +0100

    Fix UUID column error in membership seeder

--- server/src/seeders/20171202141325-existing-memberships.js
+++ server/src/seeders/20171202141325-existing-memberships.js
@@ -1,8 +1,9 @@
 module.exports = {
-  up: (queryInterface) => {
+  up: (queryInterface, Sequelize) => {
     return queryInterface.bulkInsert('Memberships',
       [
         {
+          id: Sequelize.UUIDV1,
           memberId: '75b936c0-ba72-11e7-84e1-058ffffd96c5',
           groupId: 'c46ebe90-bd68-11e7-922f-4d48c5331440',
           userRole: 'admin',
@@ -10,6 +11,7 @@ module.exports = {
           updatedAt: new Date()
         },
         {
+          id: Sequelize.UUIDV1,
           memberId: '9ee489d0-9c6f-11e7-a4d2-3b6a4940d978',
           groupId: 'c46ebe90-bd68-11e7-922f-4d48c5331440',
           userRole: 'member',
