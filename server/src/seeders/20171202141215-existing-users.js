e8110e966e91b418da42e1714d8b0dd3341395ad
Philipeano <philipeano@gmail.com>
2017-12-02 16:39:54 +0100

    Create seeders for test database

---
+++ server/src/seeders/20171202141215-existing-users.js
@@ -0,0 +1,33 @@
+const bcrypt = require('bcrypt');
+
+const salt = bcrypt.genSaltSync(3);
+const hashedPassword = bcrypt.hashSync('P@55w0rd', salt);
+
+module.exports = {
+  up(queryInterface) {
+    return queryInterface.bulkInsert('Users',
+      [
+        {
+          id: '75b936c0-ba72-11e7-84e1-058ffffd96c5',
+          username: 'philnewman',
+          password: hashedPassword,
+          email: 'philnewman@gmail.com',
+        },
+        {
+          id: '50bd1190-a116-11e7-8614-2b874e7804bc',
+          username: 'vicky',
+          password: hashedPassword,
+          email: 'vicky@gmail.com',
+        },
+        {
+          id: '9ee489d0-9c6f-11e7-a4d2-3b6a4940d978',
+          username: 'sammy',
+          password: hashedPassword,
+          email: 'sammy@gmail.com',
+        },
+      ], {});
+  },
+  down(queryInterface) {
+    return queryInterface.bulkDelete('Users', null, {});
+  }
+};
